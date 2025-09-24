bf95e16f22c406652979425776d7a5b3a39d861a
imranazeb <imranazeb@yahoo.com>
2025-09-23 17:34:48 -0700

    Fix CSRF trusted origins, enhance Celery entrypoint, and add superuser creation in start script

--- docker/local/django/start.sh
+++ docker/local/django/start.sh
@@ -12,4 +12,9 @@ python manage.py migrate --no-input
 
 python manage.py collectstatic --no-input
 
+#create superuser if not exists with email admin@example.com and password admin1234
+# If the user already exists, ignore the error
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(email='admin@example.com').exists() or User.objects.create_superuser(username='admin', email='admin@example.com', password='admin123')" | python manage.py shell
+
+
 python manage.py runserver 0.0.0.0:8000
